9f123dbc2096fca19b327406c67e88dea9cf2d3d
Keyo <gideon.chung@Outlook.com>
2024-01-03 16:22:33 +1100

    adjusted code as per instructions from notes

--- src/ERC20.sol
+++ src/ERC20.sol
@@ -19,42 +19,26 @@ Before a transferFrom can be done, the caller must first approve the contract to
 
 
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13; //define the solidity version
+pragma solidity ^0.8.19; //define the solidity version
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //shortened via the remappings.txt
 
 contract TokenTransfer {
-    IERC20 public token; //public state variable token of type IERC20 declared
-    address public sender; //public state variable sender of type address declared
-    address public recipient;
-    uint public amount;
-    //The constructor is executed when the contract is deployed. 
-    //It initializes the state variables with the provided values.
-    constructor(address _token, address _sender, address _recipient, uint _amount) {
-        token = IERC20(_token);
-        sender = _sender;
-        recipient = _recipient;
-        amount = _amount;
-    }
+
+    constructor() {}
+
     //This function performs prior check to ensure the sender is an autorized sender and has sufficient balance. If it passes, it will be sent to the _safeTransfer function
-    function transfer_check() public {
-        require(msg.sender == sender, "Not authorized"); //checks if the caller is the authorized sender. require = if true, it will continue the flow, if error, it will stop and show the error message
-        require(token.balanceOf(sender) >= amount, "Insufficient balance"); //checks if the sender has a sufficient balance of tokens
-        require(token.approve(sender,amount), "Transfer is not approved");
-        //If the checks pass, it calls the _safeTransfer function to execute the actual token transfer.
-        _safeTransfer(token, sender, recipient, amount);
+
+    function transfer(IERC20 _token, address _sender, address _recipient, uint256 _amount) public {
+        require(msg.sender == _sender, "Not authorized"); 
+        require(_token.balanceOf(_sender) >= _amount, "Insufficient balance"); 
+        require(_token.approve(_sender, _amount), "Transfer is not approved"); 
+        _safeTransfer(_token, _sender, _recipient, _amount); 
     }
-    //This is a private helper function for performing the actual token transfer.
-    function _safeTransfer(
-        IERC20 token, //It takes the ERC-20 token instance, sender, recipient, and amount as parameters.
-        address sender,
-        address recipient,
-        uint amount
-    ) private {
-        //It calls the transferFrom function on the ERC-20 token contract to transfer tokens from the sender to the recipient.
-        bool sent = token.transferFrom(sender, recipient, amount);
-        //It includes a require statement to check if the transfer was successful. 
-        //If not, it raises an error indicating that the token transfer failed.
+
+    function _safeTransfer(IERC20 _token, address _sender, address _recipient, uint256 _amount) private {
+        bool sent = _token.transferFrom(_sender, _recipient, _amount); 
         require(sent, "Token transfer failed");
     }
+    
 }
--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -1,42 +1,60 @@
 
-// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
-pragma solidity ^0.8.21;
+import "forge-std/Test.sol";
+import "../src/ERC20.sol";
+import "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {Test} from "forge-std/Test.sol";
-import 'src/ERC20.sol';
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import 'src/testToken.sol';
+contract Univ2Test is Test {
+    TokenTransfer testContract;
 
-contract ERC20test is Test{
-    ERC20 public tokenTransfer;
-    IERC20 public token;
-
-    address public sender;
-    address public recipient;
-    uint public amount = 1000;
-    uint constant SENDER_PRIVATE_KEY = 123;
-
+    // creates the contract and deploy it in a simulated environment
+    // note that this executes the constructor() method.
     function setUp() public {
-
-        sender = vm.addr(SENDER_PRIVATE_KEY);   
-        recipient = address(2);
-        // Deploy the ERC20 token contract
-        token = new ERC20("testToken", "TEST");
-
-        // Mint some tokens for the sender
-        token.mint(sender, amount);
-
-        // Create a new instance of TokenTransfer
-        tokenTransfer = new TokenTransfer(address(token), sender, recipient, amount);
+        testContract = new TokenTransfer();
     }
 
-    function testTransfer_check() public {
-        // Call the transfer_check function
-        tokenTransfer.transfer_check();
+    
+    function testDeploy() public {
+        assertTrue(address(executioner) != address(0)); //In Ethereum, a contract is considered successfully deployed if it has an address that is not the zero address.
+    }   
+    
+    function testTransferCheck() public {
+        // Declare the test context
+        address token = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // WETH https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2hk
+        address recipient = address(0xFD223351aa0Bca26D2C81f949e07d57448B132E9); // some random address
+        uint256 testBalance = 1000000000000000000; // 1 ETH
+        uint256 swapAmount = 100000000000000000; // 0.1 ETH
 
-        // Assert that the transfer was successful
-        assertEq(token.balanceOf(recipient), amount, "Incorrect recipient balance");
+        // address(this) --> address of the test account
+        // you don't need to setup a test account, the test contract will run with a random account
+        // you can access the address of the test account with address(this)
+        // cheatcode for dealing balance of a token into an address, which in this case is dealing x amount of Token to the test contract account
+        deal(token, address(this), testBalance);
+
+        // you can use the console to print out the balance of the test account
+        // just import "forge-std/console.sol" in your tests
+        // you can also import "forge-std/console.sol" into your contracts to print stuff out
+        // but make sure to remove it before deploying since it's not needed in production
+
+        console.log(
+            "Initial Sender Balance: %s Recipeient Balance:",
+            IERC20(token).balanceOf(address(this)),
+            IERC20(token).balanceOf(recipient)
+        );
+
+        // Ideally i want to call something like this:
+        testContract.transfer(IERC20(token), address(this), recipient, swapAmount);
+
+        console.log(
+            "Final Sender Balance: %s Recipeient Balance:",
+            IERC20(token).balanceOf(address(this)),
+            IERC20(token).balanceOf(recipient)
+        );
+
+        // you can use the assert functions to check if the test passed or failed
+        assertEq(IERC20(token).balanceOf(address(this)), testBalance - swapAmount);
+    
     }
 }
- 
